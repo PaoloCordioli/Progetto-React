d4c3fb6765f34d14740a0edee644c8cee1187fb0
PaoloCordioli <p.cordioli@hotmail.it>
2019-12-20 13:19:03 +0100

    Aggiunta di elementi e visualizzazione di questi tramite una lista

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import './css/bootstrap/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: "",
+      lista: []
+    }
+  }
+
+  getText = (e) => {
+    console.log(e.target.value)
+    this.setState({ value: e.target.value });
+  }
+
+  save = () => {
+     this.setState({ lista: this.state.lista.concat(this.state.value) })
+  }
+
+  render() {
+    return (
+      <div style={{marginLeft : '20px'}}>
+        <h1 style={{textAlign: 'center' }}>Todo List</h1>
+        <div>
+          <ul>
+            {this.state.lista.map(item => (
+              <li key={item} > {item}</li>
+            ))}
+          </ul>
+        </div>
+        <form>
+          <input type="text" onChange={this.getText}></input> <br></br> <br></br>
+          <button type="button" onClick={this.save}>Aggiungi</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
